053a5ec09d7438de7cdb1bfeb8eb3d83e4e0a5bd
Vilmer Fall <Vilmer.fall@gmail.com>
2025-06-19 22:06:04 +0200

    header done

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "../css/header.css";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<div className="header-inner">
+				<img src="src/assets/logo.svg" alt="Norrkatalogens logga" />
+				<h1>ADMIN</h1>
+			</div>
+		</header>
+	)
+}
+
+export default Header
