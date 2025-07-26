30c3ef103561e1a77efe0507e0841e62ef95972c
Vilmer Fall <Vilmer.fall@gmail.com>
2025-07-26 02:30:54 +0200

    added logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
 import "../css/header.css";
+import { signOut } from "firebase/auth";
+import { auth } from "../data/firebase";
+import useAuthStore from "../stores/authStore";
 
 const Header = () => {
+	const { user } = useAuthStore();
+	const handleLogout = () => {
+		signOut(auth)
+	}
+
 	return (
 		<header className="header">
 			<div className="header-inner">
 				<img src="src/assets/logo.svg" alt="Norrkatalogens logga" />
 				<h1>ADMIN</h1>
 			</div>
+			{user && <button className="logout-btn" onClick={handleLogout}> Logga ut </button>}
 		</header>
 	)
 }
