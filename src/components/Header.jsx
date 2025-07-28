41b4ccf5a24999d36cf2e9f79045ae7b72887bae
Vilmer Fall <vilmer.fall@gmail.com>
2025-07-28 16:14:18 +0200

    Better UI for userdisplay

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,9 +16,14 @@ const Header = () => {
 				<h1>ADMIN</h1>
 			</div>
 			<div className="user-info-container">
-			{user && <p className="user-info">Inloggad som: {user.email.split('@')[0]}</p>}
+				{user && (
+					<p className="user-info">
+						Inloggad som: {user.email.split('@')[0].split('.')[0].charAt(0).toUpperCase() + user.email.split('@')[0].split('.')[0].slice(1)}
+					</p>
+				)}
 
-			{user && <button className="logout-btn" onClick={handleLogout}> Logga ut </button>}
+
+				{user && <button className="logout-btn" onClick={handleLogout}> Logga ut </button>}
 
 			</div>
 		</header>
